6bfe7123439ed5c65446c879579e37ec53f5fa17
Binod Pant <binodpant.nep@gmail.com>
2019-03-02 14:49:21 +0545

    remove conflict

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-      Hello form react app
+
+          heheheh
 
         </header>
       </div>
